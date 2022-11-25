b46780e9c58c97e4b573fb510eb17cdf8ba6490e
TeeKayg <Tolunaykay@gmail.com>
2022-11-25 02:14:50 +0000

    Calculator is now working with equals button

--- js.js
+++ js.js
@@ -8,6 +8,9 @@ const totalDisplay = document.querySelector(".screenTotal");
 
 let screenTotal = '';
 let screenInput = '';
+let operator = '';
+
+
 
 numbers.forEach((btn) =>{
     btn.addEventListener("click" , (e) => {
@@ -19,4 +22,60 @@ function showNumber(num){
     screenInput += num;
     inputDisplay.textContent = screenInput;
 };
+
+operators.forEach((btn) =>{
+    btn.addEventListener("click",(e) =>{
+        addOperator(e.target.textContent);
+        calculate;
+    });
+});
+
+function addOperator(op){
+    operator = op;
+    screenTotal = screenInput;
+    screenInput = '';
+    inputDisplay.textContent = '';
+    totalDisplay.textContent = screenTotal + ' ' + operator;
+
+}
+
+AC.addEventListener('click', () => {
+    screenInput = '';
+    inputDisplay.textContent = '';
+    screenTotal = '';
+    totalDisplay.textContent = '';
+});
+
+DEL.addEventListener('click', (screenInput) => {
+    let screenInputDelete = screenInput.replace(screenInput.length-1, '');
+    inputDisplay.textContent = screenInputDelete;
+    return screenInputDelete;
+
+})
+
+equals.addEventListener("click", calculate);
+
+function calculate() {
+    screenTotal = Number(screenTotal);
+    screenInput = Number(screenInput);
+
+    if (operator === '*'){
+        screenTotal = screenInput * screenTotal;
+    };
+    if (operator === '÷'){
+        screenTotal = screenTotal / screenInput;
+    };
+    if (operator === '+'){
+        screenTotal = screenTotal + screenInput
+    };
+    if (operator === '-'){
+        screenTotal = screenTotal - screenInput;
+    };
+    totalDisplay.textContent = screenTotal;
+    inputDisplay.textContent = '';
+    screenInput = '';
+    
+}
+
+
 
